ec3375a19e9bf6bea2eb5a8c1e81a24546e08bd3
zakgul-bbc <Lara.Akguen@bbcag.ch>
2018-06-26 10:30:52 +0200

    Bestellungsdao done

---
+++ src/DAO/Bestellung.java
@@ -0,0 +1,42 @@
+package DAO;
+
+import java.util.Date;
+
+public class Bestellung {
+	private int bsID;
+	private String bestellnr;
+	private String status;
+	private Date bearbeitung;
+	private Date lieferunngsge;
+
+	public int getBsID() {
+		return bsID;
+	}
+	public void setBsID(int bsID) {
+		this.bsID = bsID;
+	}
+	public String getBestellnr() {
+		return bestellnr;
+	}
+	public void setBestellnr(String bestellnr) {
+		this.bestellnr = bestellnr;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Date getBearbeitung() {
+		return bearbeitung;
+	}
+	public void setBearbeitung(Date bearbeitung) {
+		this.bearbeitung = bearbeitung;
+	}
+	public Date getLieferunngsge() {
+		return lieferunngsge;
+	}
+	public void setLieferunngsge(Date lieferunngsge) {
+		this.lieferunngsge = lieferunngsge;
+	}
+}
